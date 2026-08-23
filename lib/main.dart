2b7be8673333e11022e5a4189154afa6b8b65ae2
zamirnaqvi <zamirnaqvi51272@gmail.com>
2026-08-23 17:38:28 +0500

    Create basic Flutter app structure
    Initial implementation of a Flutter app with a main page.

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+DRAT
+  import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyWallet',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MyWallet')),
+      body: const Center(child: Text('Hello MyWallet!')),
+    );
+  }
+}
